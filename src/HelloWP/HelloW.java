be39ec8f857b9d353b8bdcd6865dc238786fc11e
RibeiroGRVRS <ribtrab@gmail.com>
2019-08-12 20:58:45 -0300

    Segundo Commit

--- src/HelloWP/HelloW.java
+++ src/HelloWP/HelloW.java
@@ -3,8 +3,13 @@ public class HelloW {
 
 	
 	
-	public static void main(String[] args){		
-		System.out.println("Helllo word!");		
+	public static void main(String[] args){	
+		
+		System.out.println("Helllo word!");
+		for (int i=1; i< 100; i++){
+			System.out.println("Helllo word!" + i);
+		}
+		
 	}
 }
 
