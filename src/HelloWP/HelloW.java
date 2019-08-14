094d355bda80ff756859d7fd4ab3fa5dbb5031db
RibeiroGRVRS <ribtrab@gmail.com>
2019-08-14 20:15:03 -0300

    Hi

--- src/HelloWP/HelloW.java
+++ src/HelloWP/HelloW.java
@@ -5,10 +5,12 @@ public class HelloW {
 	
 	public static void main(String[] args){	
 		
-		System.out.println("Helllo word!");
-		for (int i=1; i< 100; i++){
-			System.out.println("Helllo word!" + i);
-		}
+		Usuario usuario  = New Usuario();		
+		
+		
+		/*usuario.setNome("Ricardo");		
+		System.out.println(usuario.getNome());*/
+
 		
 	}
 }
